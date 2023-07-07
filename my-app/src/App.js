414865f80aa33875c5b345fd3b5be58390ae6e7e
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-07 18:31:14 -0500

    3D Sculpture Imported

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,13 @@
 import React, { useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useLoader } from '@react-three/fiber';
 import './App.css';
+
+function Model() {
+  const gltf = useLoader(GLTFLoader, '/cesar_-_louvre_museum/scene.gltf');
+  return <primitive object={gltf.scene} />;
+}
 
 function App() {
   // Set the background to pitch black when component mounts
@@ -20,12 +28,18 @@ function App() {
       </div>
 
       <div className="about">
-        <p>Hi, I'm Adrianna Pinzariu.
-        I'm a web developer with a passion for creating beautiful and functional websites.
-        With expertise in React.js, CSS and HTML, I strive to make the web a more user-friendly place. Hi, I'm Adrianna Pinzariu.
-        I'm a web developer with a passion for creating beautiful and functional websites.
-       </p>
+        <p>
+          Hi, I'm Adrianna Pinzariu. I'm a web developer with a passion for creating beautiful and
+          functional websites. With expertise in React.js, CSS, and HTML, I strive to make the web a
+          more user-friendly place. Hi, I'm Adrianna Pinzariu. I'm a web developer with a passion for
+          creating beautiful and functional websites.
+        </p>
       </div>
+
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <Model />
+      </Canvas>
     </div>
   );
 }
