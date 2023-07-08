9edb80173cc1642ddd27370aee1e7df2bdf2f9a8
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-07-07 20:55:46 -0500

    Text on Top of Model

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,9 +7,9 @@ import { MeshStandardMaterial, Color } from 'three';
 import './App.css';
 
 const Model = () => {
-  const mesh = useRef();
-  const obj = useLoader(OBJLoader, '/Venus_de_Milo_SMK_KAS434_1.obj')
+  const gltf = useLoader(GLTFLoader, '/cesar_-_louvre_museum/scene.gltf', (loader) => loader.manager.onError = (url, error) => console.error('Error loading', url, error));
 
+  if (!gltf) return null;
   // Create a dark grey MeshStandardMaterial
   const material = new MeshStandardMaterial({ 
     color: new Color(0x777777), 
@@ -18,53 +18,18 @@ const Model = () => {
   });
 
   // Traverse the model and update the material on each mesh
-  obj.traverse((child) => {
-    if (child.isMesh) {
-      child.material = material;
-    }
-  });
-
-  useFrame(({ clock }) => {
-    if (mesh.current) {
-      mesh.current.rotation.y = clock.getElapsedTime() * 0.5;
-    }
-  });
-
-  return (
-    <mesh ref={mesh} scale={0.0075} position={[0, -15, 0]}>
-      <primitive object={obj} />
-    </mesh>
-  );
-}
-/*const Model = () => {
-  const meshRef = useRef();
-  const gltf = useLoader(GLTFLoader, '/cesar_-_louvre_museum/scene.gltf');
-
-  const material = new MeshStandardMaterial({
-    color: new Color(0x777777),
-    roughness: 0.5,
-    metalness: 1,
-  });
-
   gltf.scene.traverse((child) => {
     if (child.isMesh) {
       child.material = material;
     }
   });
 
-  useFrame(({ clock }) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 0.01; // adjust speed here
-    }
-  });
-
   return (
-    <mesh ref={meshRef} scale={7} position={[0, -3, 1]} rotation={[0.125, 5.175, 0]}>
+    <mesh scale={7} position={[0, -3, 1]} rotation={[0.125, 5.175, 0]}>
       <primitive object={gltf.scene} />
     </mesh>
   );
-};*/
-
+}
 
 function App() {
   useEffect(() => {
@@ -78,12 +43,20 @@ function App() {
       <div className="line" />
       <div className="line" />
 
-      <div className="text">
+      <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
+        <Canvas>
+          <ambientLight intensity={0.9} />
+          <directionalLight intensity={.5} position={[0, 10, 5]} /> 
+          <Model />
+        </Canvas>
+      </div>
+
+      <div className="text" style={{ position: 'absolute', right: '40%', marginTop: '20vh' }}>
         <div>Adrianna</div>
         <div>Pinzariu</div>
       </div>
 
-      <div className="about">
+      <div className="about" style={{ position: 'absolute', width: '15%', left: '62.5%', marginTop: '23vh' }}>
         <p>
           Hi, I'm Adrianna Pinzariu. I'm a web developer with a passion for creating beautiful and
           functional websites. With expertise in React.js, CSS, and HTML, I strive to make the web a
@@ -91,12 +64,6 @@ function App() {
           creating beautiful and functional websites.
         </p>
       </div>
-
-      <Canvas>
-  <ambientLight intensity={0.5} />
-  <directionalLight intensity={1} position={[0, 10, 5]} /> 
-  <Model />
-</Canvas>
     </div>
   );
 }
